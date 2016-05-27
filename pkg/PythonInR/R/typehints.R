9dedb5d707329f54527d5e8b40c9454ee54b7f66
fschwendinger <fschwendinger@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-05-27 07:41:32 +0000

    update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pythoninr@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/PythonInR/R/typehints.R
+++ pkg/PythonInR/R/typehints.R
@@ -1,5 +1,3 @@
-
-
 th.int <- function(x) tyhi(x, "int")
 th.long <- function(x) tyhi(x, "long")
 th.float <- function(x) tyhi(x, "float")
@@ -9,11 +7,25 @@ th.string <- function(x) tyhi(x, "string")
 th.bytes <- function(x) tyhi(x, "bytes")
 th.unicode <- function(x) tyhi(x, "unicode")
 
+th.vector <- function(x) tyhi(x, "vector")
+
 th.list <- function(x) tyhi(x, "list")
 th.tuple <- function(x) tyhi(x, "tuple")
 th.tuple_int <- function(x) tyhi(x, c("tuple", "int"))
 
 th.numpy <- function(x) tyhi(x, "numpy")
-th.cvxopt <- function(x) tyhi(x, "cvxopt")
+th.cvxopt <- function(x) {
+	if ( is.matrix(x) & is.numeric(x) ) {
+		if (is.integer(x)) return(tyhi(x, c("cvxopt", "int")))
+		return(tyhi(x, "cvxopt"))
+	}		
+	if ( is.simple_triplet_matrix(x) ) {
+		if ( is.numeric(x$v) ) {
+			if (is.integer(x)) return(tyhi(x, c("cvxopt", "int")))
+			return(tyhi(x, "cvxopt"))
+		}
+	}
+	stop("TYPE_ERROR in 'th.cvxopt': cvxopt matrices have to be of type 'double'!")
+}
 
 th.pandas <- function(x) tyhi(x, "pandas")
