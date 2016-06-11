6206584c0ccaa3a4bf704632eab414a6e359610b
fschwendinger <fschwendinger@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-06-11 14:45:13 +0000

    add Operators
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pythoninr@29 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/PythonInR/devel/Operators.R
@@ -0,0 +1,26 @@
+library("PythonInR")
+
+pySet("x", th.numpy(1:3))
+pySet("y", th.numpy(2:4))
+class(pyGet0("x"))
+
+`+.ndarray` <- function(x, y) {
+	pyGet0(sprintf("%s + %s", x$py.variableName, y$py.variableName))
+}
+
+x <- pyGet0("x")
+y <- pyGet0("y")
+
+class(x + y)
+x + y
+
+
+as.list
+
+PythonInR:::pyIsCallable
+
+e <- pyGet("z")
+e
+class()
+capture.output(tryCatch(z <- pyGet("z"), error=function(e) NULL))
+z
