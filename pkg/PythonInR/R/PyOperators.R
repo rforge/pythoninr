705ea2a88790a2637cdbc4e83ed8bb2819820b4b
fschwendinger <fschwendinger@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-06-24 21:53:42 +0000

    add PyOperators
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pythoninr@37 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/PythonInR/R/PyOperators.R
@@ -0,0 +1,27 @@
+
+is.PythonInR_Object <- function(x) inherits(x, "PythonInR_Object")
+
+pyOperator <- function(operator, a, b) {
+    tmp_name <- "__R__.namespace['__temp__']"
+    if ( is.PythonInR_Object(a) & !is.PythonInR_Object(b) ) {
+        pySet("__temp__", b, namespace="__R__.namespace")
+        return(pyGet(sprintf("%s %s %s", pyRef(a), operator, tmp_name), FALSE))
+    }
+    if ( !is.PythonInR_Object(a) & is.PythonInR_Object(b) ) {
+        pySet("__temp__", a, namespace="__R__.namespace")
+        return(pyGet(sprintf("%s %s %s", tmp_name, operator, pyRef(b)), FALSE))
+    }
+    return( pyGet(sprintf("%s %s %s", pyRef(a), operator, pyRef(b)), FALSE) )
+}
+
+`+.PythonInR_Object` <- function(a, b) pyOperator("+", a, b)
+`*.PythonInR_Object` <- function(a, b) pyOperator("*", a, b)
+`-.PythonInR_Object` <- function(a, b) pyOperator("-", a, b)
+`/.PythonInR_Object` <- function(a, b) pyOperator("/", a, b)
+
+pyRef <- function(x) {
+    if ( inherits(x, "pyFunction") ) return(attr(x, "name"))
+    if ( inherits(x, "PythonInR_Object") ) return(x$.name)
+    NULL
+}
+
