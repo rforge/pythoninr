1f12b56489571b92f3b2da42ffd3cac67210f936
fschwendinger <fschwendinger@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-01-08 20:00:38 +0000

    update Tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pythoninr@72 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/PythonInR/tests/NLP_Tree.R
+++ pkg/PythonInR/tests/NLP_Tree.R
@@ -9,6 +9,6 @@ library(testthat)
 
 vp <- Tree('VP', list(Tree('V', list('saw')), Tree('NP', list('him'))))
 pySet("x", vp)
-expect_equal(pyType("x"), "Tree")
+expect_true(pyType("x") %in% c("Tree", "PythonInR.Tree"))
 expect_equal(pyGet("x"), vp)
 
